e4bb43aa508c184e923254d8296fca2dacd38d10
kodonghyun <kodonghyun1209@gmail.com>
2017-04-27 21:18:52 +0900

    유저 모델 수정
    비밀번호 체크 메서드 추가

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 
 const Sequelize = require('sequelize');
+const crypto = require('crypto');
 
 const sequelize = require('./../config/env/sequelize.js');
 
@@ -18,6 +19,15 @@ const User = sequelize.define('user', {
     allowNull: false,
   },
 }, {
+  instanceMethods: {
+    // 비밀번호를 해시값으로 변경하는 메서드
+    validPassword: function (password, salt) {
+      if (!password || !salt)
+        return '';
+      salt = new Buffer(salt, 'base64');
+      return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+    },
+  },
   timestamps: true,
   paranoid: true,
   underscored: true,
