cb376461484b2c5bd3ac1d4dc6e5281b7369cf0a
kodonghyun <kodonghyun1209@gmail.com>
2017-04-28 07:43:43 +0900

    스케줄 조회 유효성 체크 추

--- routes/validation/validation.js
+++ routes/validation/validation.js
@@ -34,3 +34,43 @@ exports.signIn = function(nickname, password) {
     resolve();
   });
 };
+
+
+/**
+ * 스케줄 조회 유효성 검사
+ *
+ * @param userId
+ */
+exports.getSchedule = function(userId) {
+  return new Promise(function(resolve, reject) {
+    if (userId === undefined) {
+      return reject(new helper.makePredictableError(200, '필요한 파라미터를 다 받지 못했습니다.'));
+    }
+
+    resolve();
+  });
+};
+
+
+/**
+ * 스케줄 조회 유효성 검사
+ *
+ * @param userId
+ * @param monday
+ * @param tuesday
+ * @param wednesday
+ * @param thursday
+ * @param friday
+ * @param saturday
+ * @param sunday
+ */
+exports.save = function(userId, monday, tuesday, wednesday, thursday, friday, saturday, sunday) {
+  return new Promise(function(resolve, reject) {
+    if (userId === undefined || monday === undefined || tuesday === undefined || wednesday === undefined
+      || thursday === undefined || friday === undefined || saturday === undefined || sunday === undefined) {
+      return reject(new helper.makePredictableError(200, '필요한 파라미터를 다 받지 못했습니다.'));
+    }
+
+    resolve();
+  });
+};
