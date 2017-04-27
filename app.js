52e7c4590d352d60c62f17fb56db0cbddd7d311f
kodonghyun <kodonghyun1209@gmail.com>
2017-04-28 01:18:08 +0900

    에러 발생 시 처리부분 수정
    200번으로 에러를 던지도록

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if (err.statusCode === 200) {
+    return res.json({
+      success: 0,
+      result: err.message,
+    })
+  }
   // render the error page
   res.status(err.status || 500);
   res.render('error');
