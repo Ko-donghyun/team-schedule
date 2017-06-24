a04335bbd967edd19bb249b24bb368311512aadb
kodonghyun <kodonghyun1209@gmail.com>
2017-06-24 15:57:43 +0900

    유저 프로필 저장을 위한 헬퍼 메서드 추가
    1. 받은 파일을 저장하고, 이름을 변경
    2. 저장된 파일을 S3에 업로드
    3. S3에 올라갔으므로 local 파일 제거

--- routes/helper/helper.js
+++ routes/helper/helper.js
@@ -1,5 +1,14 @@
 
+const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
+
+const randomString = require("randomstring");
+const AWS = require('aws-sdk');
+const credentials = require('./../../credentials.js');
+
+const s3 = new AWS.S3({accessKeyId: credentials.s3AccessKey, secretAccessKey: credentials.s3SecretKey});
+const s3Bucket = credentials.s3Bucket;
 
 /**
  * 예측 가능한 에러를 리턴하는 메서드
@@ -37,3 +46,46 @@ exports.checkPassword = function(password, salt, encrypt_password) {
   return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64') === encrypt_password;
 };
 
+exports.saveProfileImage = function(user, profileImage) {
+  return new Promise((resolve, reject) => {
+    const _this = this;
+    const randomFileName = new Date().getTime() + randomString.generate(7);
+    const filePath = __dirname + '/../../public/images/' + randomFileName + path.extname(profileImage.path);
+
+    fs.rename(profileImage.path, filePath, (err) => {
+      if (err) {
+        return reject(new _this.makePredictableError(200, '파일 저장 실패했습니다.'));
+      }
+
+      console.log('S3 저장 시작');
+      const params = {Bucket: s3Bucket, Key: randomFileName + path.extname(profileImage.path),
+        ACL: 'public-read',
+        ContentType: profileImage.type,
+        Body: fs.createReadStream(filePath)};
+      return s3.upload(params, function(err, data) {
+        if (err) {
+          console.log(err);
+          return reject(new _this.makePredictableError(200, 'S3 파일 저장 실패했습니다.'));
+        } else {
+          console.log('S3 저장 완료');
+
+          user.profile = data.Location;
+
+          fs.access(filePath, fs.F_OK, function(err) {
+            if (err) {
+              return reject(new _this.makePredictableError(200, '로컬 파일 삭제 실패했습니다.'));
+            }
+
+            fs.unlink(filePath, function(err) {
+              if (err) {
+                return reject(new _this.makePredictableError(200, '로컬 파일 삭제 실패했습니다.'));
+              } else {
+                resolve(user);
+              }
+            });
+          });
+        }
+      });
+    });
+  });
+};
